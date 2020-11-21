13b679520698cd708e7814864c0bc5de32c15e8b
comav <tsyomav@gmail.com>
2020-11-21 19:13:40 +0200

    Layout is now working

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+function changeTab (tabId) {
+    let tabs = Array.from(document.getElementsByClassName('tab'));
+    currentTab = document.getElementById(tabId);
+    for (let i = 0; i<tabs.length; i++) {
+        console.log('jjjj', tabs[i])
+        tabs[i].style.display = 'none';
+    }
+    currentTab.style.display = 'block';
+}
